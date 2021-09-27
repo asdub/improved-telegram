ff39be7a9bba5555f67527c940a45e7dcdbd3f2f
asdub <adam.smitzer@gmail.com>
2021-09-27 16:12:28 +0000

    Removed django.urls from urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ View to show products and sorting """
+
+    products = Product.objects.all()
+
+    context = {
+        'products' : products,
+    }
+    
+    return render(request, 'products/products.html', context)
