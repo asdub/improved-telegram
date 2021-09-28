fb854d7f8d2e3bbd1703cc3bfb697773b395bef9
asdub <adam.smitzer@gmail.com>
2021-09-28 14:57:58 +0000

    Added url.py for shopping bag, updated hrefs on base.html & mobile nav
    Updated top level url.py in improved design dir

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_bag(request):
+    """ View to render shopping bag order contents """
+
+    return render(request, 'bag/bag.html')
