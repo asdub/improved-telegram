f1f41df980b8bd7f12cb03497394dcf79076016a
asdub <adam.smitzer@gmail.com>
2021-09-29 10:44:06 +0000

    Added artwork order fields to product_detail.html
    Updated bag views for new form data

--- bag/views.py
+++ bag/views.py
@@ -14,6 +14,20 @@ def add_to_order(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    customer_order = None
+
+    if 'artwork_request' in request.POST:
+        artwork_request = request.POST['artwork_request']
+        product_size = request.POST['product_size']
+        product_text_content = request.POST['product_text_content']
+        artwork_colour = request.POST['artwork_colour']
+        
+        customer_order = ({'item_id': item_id})
+        customer_order.update({'artwork_request': artwork_request})
+        customer_order.update({'product_size': product_size})
+        customer_order.update({'product_text_content': product_text_content})
+        customer_order.update({'artwork_colour': artwork_colour})
+
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -22,4 +36,5 @@ def add_to_order(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(customer_order)
     return redirect(redirect_url)
