81928917746e164b9c78fc5f96544557f031b64f
asdub <adam.smitzer@gmail.com>
2021-10-01 14:47:28 +0000

    Amended logic in contexts.py to handle customer custom requqests
    Amended views.py to create a customer order dict within a list of orders

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,17 +10,19 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    print(f'BAG----->>> {bag}')
     order = request.session.get('order')
+    print(f'ORDER----->>> {order}')
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    if order is not None:
+        for data in order:
+            print(data['item_id'])
+            print(data['quantity'])
+
+    if order is not None:
+        for data in order:
+            product = get_object_or_404(Product, pk=data['item_id'])
+            data['product'] = product
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -32,7 +34,7 @@ def bag_contents(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
+        'order': order,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
@@ -40,5 +42,4 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from . models import *
 
 # Create your views here.
 
@@ -13,32 +12,22 @@ def view_bag(request):
 def add_to_order(request, item_id):
     """ Add quantity of the specified product/ service to the bag """
 
-    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    order = {}
-    order['product_id'] = item_id
-    order['quantity'] = quantity
+    order = request.session.get('order')
+    cus_order = {
+            "item_id": item_id,
+            "quantity": int(request.POST.get("quantity")),
+            "artwork_request": request.POST.get("artwork_request"),
+            "product_size": request.POST.get("product_size"),
+            "product_text_content": request.POST.get("product_text_content"),
+            "artwork_colour": request.POST.get("artwork_colour")
+        }
 
-    if 'artwork_request' in request.POST:
-        order['artwork_request'] = request.POST['artwork_request']
-        if 'product_size' in request.POST:
-            order['product_size'] = request.POST['product_size']
-            if 'product_text_content' in request.POST:
-                order['product_text_content'] = request.POST['product_text_content']
-                if 'artwork_colour' in request.POST:
-                    order['artwork_colour'] = request.POST['artwork_colour']
-
-    bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        order['quantity'] += quantity
-        bag[item_id] += quantity
+    if order is not None:
+        order.append(cus_order.copy())
     else:
-        bag[item_id] = quantity
+        order = []
+        order.append(cus_order.copy())
 
-    request.session['bag'] = bag
     request.session['order'] = order
-    print(order)
-    print(quantity)
-    print(order['quantity'])
     return redirect(redirect_url)
