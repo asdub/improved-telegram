6d00c5d3104313df94186f60d263e4d274aa7af0
asdub <adam.smitzer@gmail.com>
2021-10-02 17:16:04 +0000

    Added helper.js for bag qty updates and future js, including dir
    Added helper.js to postloadjs block on bag.html

---
+++ static/js/helper.js
@@ -0,0 +1,29 @@
+const csrftoken = Cookies.get('csrftoken');
+console.log(csrftoken)
+
+    function submitForm() {
+        document.getElementById('update-form').submit()
+    }
+
+    function submitFormRemove(){
+        document.getElementById('id_qty').value = 0;
+        document.getElementById('update-form').submit();
+    }
+
+    let form = document.getElementById('update-form');
+
+    form.addEventListener('submit', function(event) { 
+        event.preventDefault()
+        
+        let data = new FormData();
+        data.append('id_qty', document.getElementById('id_qty').value)  
+        data.append('csrfmiddlewaretoken', csrftoken)
+       
+        axios.post('bag/update_order', data)
+        .then(res => {
+            console.log(csrftoken)
+            location.reload()
+        })
+        .catch(errors => console.log(errors))
+
+    })
