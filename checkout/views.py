22ef33a4b6a31eca98eb653bb50d44291d277931
asdub <adam.smitzer@gmail.com>
2021-10-04 10:54:47 +0000

    Updated checkout viw to handle order form/ stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from .models import Order, OrderLineItem
+from products.models import Product
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
@@ -14,27 +16,88 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order = request.session.get('order')
-    if not order:
-        messages.error(request, "No orders found.")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        bag = request.session.get('order')
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for data in bag:
+                try:
+                    product = get_object_or_404(Product, pk=data['item_id'])
+                    if data:
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=data['qauntity'],
+                        )
+                        order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, "There was an issue processing your order. Please try again.")
+                    order_line_item.delete()
+                    return redirect(reverse('view_bag'))
 
-    )
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+   
+        else:
+            messages.error(request,
+                "There was an error processing your details.")
+    else:
+        order = request.session.get('order')
+        if not order:
+            messages.error(request, "No orders found.")
+            return redirect(reverse('products'))
 
-    order_form = OrderForm()
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+
+        )
+
+        order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'order' in request.session:
+        del request.session['order']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
