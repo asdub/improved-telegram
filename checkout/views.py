5794f760cc93a732ffab6ce2266f97e130a393f2
asdub <adam.smitzer@gmail.com>
2021-10-04 13:02:18 +0000

    style: Updated toast notification styling
    Amended toast notifcation layout

--- checkout/views.py
+++ checkout/views.py
@@ -93,8 +93,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        Your order number is {order_number}. \
+        A confirmation email will be sent to {order.email}.')
 
     if 'order' in request.session:
         del request.session['order']
