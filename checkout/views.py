8580c741bf440dfbbc2b1ad490a6416b71208e5e
asdub <adam.smitzer@gmail.com>
2021-10-04 00:10:44 +0000

    Added stripe functionality to checkout app
    fix: Cleaned up formatting in forms & views

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JgeIWFQi3WuvSnPBMD2MFkIqrArpb5ZYuZfLfCUDRLyzwCrcUInyoZfo2W3GHWy2QzDzeuZMV0mjM0haBXNorfP0081p248xP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
