49ff2929ebbded1c1719553fd78845add691139f
asdub <adam.smitzer@gmail.com>
2021-10-12 10:30:21 +0000

    Added confirmation email settings and class to chekecout views

--- improved_design/settings.py
+++ improved_design/settings.py
@@ -182,3 +182,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Email Settings
+
+DEFAULT_FROM_EMAIL = 'orders@improveddesign.com'
