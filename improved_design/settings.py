00b608ee7ec852ff70ff09043a006591906a28fc
asdub <adam.smitzer@gmail.com>
2021-10-15 13:30:57 +0000

    Removed production secret_key and set debug

--- improved_design/settings.py
+++ improved_design/settings.py
@@ -21,12 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-        'django-insecure-3@kqv-$db1$crzgltuj*u^-c3@xkgu5(h9x+5f$#z6x%8a3d4p'
-        )
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['improved-design-asdub.herokuapp.com', 'localhost']
 
